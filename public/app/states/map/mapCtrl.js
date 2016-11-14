3bff092f86882f2974c89a1e4df80ba474754150
howdycrystalm <howdycrystalm@gmail.com>
2016-11-14 15:56:50 -0700

    working on the dots, showing for each object now

--- public/app/states/map/mapCtrl.js
+++ public/app/states/map/mapCtrl.js
@@ -1,5 +1,9 @@
 angular.module('orderhound')
 .controller('mapCtrl', function($scope) {
 
+  $scope.message = "hi, lets do this";
+
+  const CHART = document.getElementById('lineChart');
+  console.log(CHART);
 
 });
